47e1d0bc118b375756c7f73895a06684723640ee
kanzah259 <kanzahhussain259@gmail.com>
2025-08-20 12:09:14 +0100

    Setup project structure and install TailwindCSS

---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+
+const Hero = () => (
+
+    <div>
+        <div className="text-4xl font-bold text-cyan-400">Kanzah Hussain</div>
+<div className="text-xl font-medium text-white">Hey</div>
+        <div className="bg-red-700 text-white p-4 text-2xl">
+    Tailwind Works 🚀
+  </div>
+    </div>
+    
+)
+    
+
+    export default Hero;
