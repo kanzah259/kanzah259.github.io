d7708064ea3a04f2721dce46cad8f896fd767524
kanzah259 <kanzahhussain259@gmail.com>
2025-08-23 22:32:23 +0100

    Style navbar

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@
 const Button = () => (
 
     <div className="text-center">
-        <button type="button" className="text-[#0A0A0A] font-orbitron bg-[#00FFFF] font-bold p-3 text-[20px] tracking-[-0.02em]">View Projects</button>
+        <button type="button" className="text-[#0A0A0A] font-orbitron bg-[#00FFFF] font-bold p-3 text-[20px] tracking-[-0.02em] hover:bg-sky-700">View Projects</button>
     </div>
 
 )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,18 @@
 
 const Navbar = () => (
 
-    <div className="text-[#E0E0E0] font-exo2 pt-4 pr-5 pl-5">
+    <div className="text-[#E0E0E0] font-exo2 pt-5 pr-6 pl-6">
         <nav className="flex justify-between">
 
-            <div className="text-[#E0E0E0] font-bold"><a href="#hero">Kanzah Hussain</a></div>
+            <div className="text-[#E0E0E0] font-bold text-[24px]"><a href="#hero">Kanzah Hussain</a></div>
 
 
-            <ul className="flex gap-5">
-                <li><a href="lolo">About</a></li>
-                <li><a href="lolo">Experience</a></li>
-                <li><a href="lolo">Projects</a></li>
-                <li><a href="lolo">Skills</a></li>
-                <li><a href="lala">Contact</a></li>
+            <ul className="flex gap-5 text-[20px]">
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">About</a></li>
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Experience</a></li>
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Projects</a></li>
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Skills</a></li>
+                <li className= "active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lala">Contact</a></li>
             </ul>
         </nav>
     </div>
