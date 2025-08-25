b8e18cc00adcd7d9d1e98eb7d676ab113f863074
kanzah259 <kanzahhussain259@gmail.com>
2025-08-25 22:12:29 +0100

    Fix styling for timeline

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -6,7 +6,8 @@ const Experience = () => (
             <h3 className="font-orbitron text-[#00FFFF] text-[70px] pl-10 ml-10 pt-10 mt-10 font-bold">Experience</h3>
         </div>
         <div className="w-full max-w-2xl mx-auto">
-            <div className="absolute left-12 ml-7 h-full border-l-2 border-[#00FFFF] shadow-[0_0_10px_#00fff]">
+            <div className="absolute left-12 ml-7 pb-7 border-l-2 border-[#00FFFF] shadow-[0_0_10px_#00fff] h-120"> 
+                {/* i deleted h-full above */}
                 <div className="relative">
                 <div className="w-4 h-4 rounded-full bg-[#00FFFF] shadow-[0_0_8px_#00fff] absolute -left-[9px] top-10"></div>
                     <h3 className="font-orbitron text-[36px] text-[#00FFFF] font-bold pl-8 pt-5">National Hackathons Management Team</h3>
@@ -14,7 +15,7 @@ const Experience = () => (
                 <p className="font-exo2 text-[#E0E0E0] text-[24px] pl-8">Supported university hackathon teams, helping deliver events with [X] participants.</p>
                 <p className="font-exo2 text-[#E0E0E0] text-[24px] pl-8">Led a 3-person publicity team, creating designs that communicated the mission to sponsors.</p> 
                 <p className="font-exo2 text-[#E0E0E0] text-[24px] pl-8">Built the event website with React, TypeScript, and Tailwind, iterating to improve UX.</p>
-                </div>
+                
 
                 <div className="relative">
                 <div className="w-4 h-4 rounded-full bg-[#00FFFF] shadow-[0_0_8px_#00fff] absolute -left-[9px] top-19 mt-10"></div>
@@ -23,6 +24,7 @@ const Experience = () => (
                 <p className="font-exo2 text-[#E0E0E0] text-[24px] pl-8">Cut Largest Contentful Paint from 4.7s → 1.0s (79% faster load times).</p>
                 <p className="font-exo2 text-[#E0E0E0] text-[24px] pl-8">Reduced Total Blocking Time from 210ms → 0ms, enabling instant interactivity.</p> 
                 <p className="font-exo2 text-[#E0E0E0] text-[24px] pl-8">Achieved 97+ Google PageSpeed scores and 100% GTmetrix performance.</p>
+                </div>
                 </div>
 
                 {/* <div className="w-4 h-4 rounded-full bg-[#00FFFF] shadow-[0_0_8px_#00fff] absolute -left-[9px] top-10"></div>
