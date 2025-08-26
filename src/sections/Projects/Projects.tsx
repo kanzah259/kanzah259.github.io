10725807213c898cb992ada056bb4e7ade550e1d
kanzah259 <kanzahhussain259@gmail.com>
2025-08-26 13:12:55 +0100

    Complete one project card styling

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -10,7 +10,24 @@ const Projects = () => (
 
             <div className="text-center bg-[#121212] border-solid border-2 border-[#00FFFF] ml-40 mt-10">
                 <h3 className="font-orbitron text-[#00FFFF] text-[36px] font-bold pt-5">Project 1</h3>
-                <p className="font-exo2 text-[#E0E0E0] text-[18px] pt-5">Full-Stack E-Commerce<br /> website for Jewellery.</p>
+                <p className="font-exo2 text-[#E0E0E0] text-[18px] pt-5 pb-5">Full-Stack E-Commerce<br /> website for Jewellery.</p>
+
+                <div className="">
+                    <div className=" flex justify-center">
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">MongoDB</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Express.js</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">React.js</h4>
+                    </div>
+
+                    <div className=" flex justify-center">
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Node.js</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">TypeScript</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Figma</h4>
+                    </div>
+                    <button type="button" className="text-[#00FFFF] font-orbitron bg-[#121212] border-solid border-[#00FFFF] border-2 font-bold p-3 mt-5 mb-5 text-[26px] tracking-[0.02em] hover:bg-sky-700">GITHUB</button>
+
+                </div>
+
             </div>
             </div>
 
@@ -19,3 +36,5 @@ const Projects = () => (
 )
 
 export default Projects;
+
+// MongoDB, Express.js, React, Node.js, Typescript, Figma  [Project 1]
