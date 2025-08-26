ae0d452b6cd52cdc8076c2d7b7f1b0be50ff203b
kanzah259 <kanzahhussain259@gmail.com>
2025-08-26 15:42:25 +0100

    Add routing to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,15 +4,15 @@ const Navbar = () => (
     <div className="text-[#E0E0E0] font-exo2 pt-5 pr-6 pl-6">
         <nav className="flex justify-between">
 
-            <div className="text-[#E0E0E0] font-bold text-[24px]"><a href="#hero">Kanzah Hussain</a></div>
+            <div className="text-[#E0E0E0] font-bold text-[24px]"><a href="#Hero">Kanzah Hussain</a></div>
 
 
             <ul className="flex gap-5 text-[20px]">
-                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">About</a></li>
-                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Experience</a></li>
-                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Projects</a></li>
-                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Skills</a></li>
-                <li className= "active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lala">Contact</a></li>
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="#About">About</a></li>
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="#Experience">Experience</a></li>
+                <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="#Projects">Projects</a></li>
+                {/* <li className="active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="lolo">Skills</a></li> */}
+                <li className= "active:text-[#00FFFF] no-underline hover:underline decoration-[#00FFFF] underline-offset-4 decoration-2"><a href="#Contact">Contact</a></li>
             </ul>
         </nav>
     </div>
