8769a3689d2fea18702b5759efc3801906ff622a
kanzah259 <kanzahhussain259@gmail.com>
2025-08-26 15:36:44 +0100

    Add Contact section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar";
 import About from "./sections/About/About";
 import Experience from "./sections/Experience/Experience";
 import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
     <About />
     <Experience />
     <Projects />
+    <Contact />
 
     </div>
 
