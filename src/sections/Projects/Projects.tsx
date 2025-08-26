5db8618f050505b3b098c2b509dccf62a6580c60
kanzah259 <kanzahhussain259@gmail.com>
2025-08-26 15:19:14 +0100

    Finish styling for project section and fix spacing bug

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -6,13 +6,14 @@ const Projects = () => (
             <h3 className="font-orbitron text-[#00FFFF] text-[70px] pl-10 ml-10 pt-10 mt-10 font-bold">Projects</h3></div>
 
         
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pl-20">
 
-            <div className="text-center bg-[#121212] border-solid border-2 border-[#00FFFF] ml-40 mt-10">
-                <h3 className="font-orbitron text-[#00FFFF] text-[36px] font-bold pt-5">Project 1</h3>
-                <p className="font-exo2 text-[#E0E0E0] text-[18px] pt-5 pb-5">Full-Stack E-Commerce<br /> website for Jewellery.</p>
+            {/* card 1 */}
+            <div className="text-center bg-[#121212] border-solid border-2 border-[#00FFFF] mt-10 w-fit p-5">
+                <h3 className="font-orbitron text-[#00FFFF] text-[36px] font-bold pt-5">Portfolio</h3>
+                <p className="font-exo2 text-[#E0E0E0] text-[18px] pt-5 pb-5">Full-Stack Portfolio website<br /> showcasing my work.</p>
 
-                <div className="">
+                
                     <div className=" flex justify-center">
                     <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">MongoDB</h4>
                     <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Express.js</h4>
@@ -26,9 +27,58 @@ const Projects = () => (
                     </div>
                     <button type="button" className="text-[#00FFFF] font-orbitron bg-[#121212] border-solid border-[#00FFFF] border-2 font-bold p-3 mt-5 mb-5 text-[26px] tracking-[0.02em] hover:bg-sky-700">GITHUB</button>
 
-                </div>
-
+                
             </div>
+
+
+
+            {/* card 2 */}
+            <div className="text-center bg-[#121212] border-solid border-2 border-[#00FFFF] mt-10 w-fit p-5">
+                <h3 className="font-orbitron text-[#00FFFF] text-[33px] font-bold pt-5">Habit Tracker</h3>
+                <p className="font-exo2 text-[#E0E0E0] text-[18px] pt-5 pb-5">Full-Stack E-Commerce<br /> website for Jewellery.</p>
+
+                
+                    <div className=" flex justify-center">
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">MongoDB</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Express.js</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">React.js</h4>
+                    </div>
+
+                    <div className=" flex justify-center">
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Node.js</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">TypeScript</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Figma</h4>
+                    </div>
+                    <button type="button" className="text-[#00FFFF] font-orbitron bg-[#121212] border-solid border-[#00FFFF] border-2 font-bold p-3 mt-5 mb-5 text-[26px] tracking-[0.02em] hover:bg-sky-700">GITHUB</button>
+
+                
+            </div>
+
+
+
+        {/* card 3 */}
+            <div className="text-center bg-[#121212] border-solid border-2 border-[#00FFFF] mt-10 w-fit p-5">
+                <h3 className="font-orbitron text-[#00FFFF] text-[36px] font-bold pt-5">Java Game</h3>
+                <p className="font-exo2 text-[#E0E0E0] text-[18px] pt-5 pb-5">Cyberpunk Game<br /> featuring 5 levels.</p>
+
+                
+                    <div className=" flex justify-center">
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Java</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Github</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Git</h4>
+                    </div>
+
+                    <div className=" flex justify-center">
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Node.js</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">TypeScript</h4>
+                    <h4 className="border-solid border-[#00FFFF] border-2 text-[#E0E0E0] rounded-full p-0.5 m-1">Figma</h4>
+                    </div>
+                    <button type="button" className="text-[#00FFFF] font-orbitron bg-[#121212] border-solid border-[#00FFFF] border-2 font-bold p-3 mt-5 mb-5 text-[26px] tracking-[0.02em] hover:bg-sky-700">GITHUB</button>
+
+                
+            </div>
+
+
             </div>
 
     </section>
